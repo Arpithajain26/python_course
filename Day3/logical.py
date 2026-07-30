64d6ddcf7a3a1921a67e29b118d251eda7ee79b5
Arpitha Jain <arpithaammujain39@gmail.com>
2026-07-30 17:07:59 +0530

    program of if-else statement

---
+++ Day3/logical.py
@@ -0,0 +1,5 @@
+# if applicant has high income and good credit then he is eligible for loan
+has_income=True
+has_good_credit=True
+if has_good_credit and has_income:
+    print("eligible for loan")
