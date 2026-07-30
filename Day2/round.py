0cc6405ce88b1f374b81b2a1fa93a0e760627afa
Arpitha Jain <arpithaammujain39@gmail.com>
2026-07-30 16:59:52 +0530

    program of if-else statement

---
+++ Day2/round.py
@@ -0,0 +1,5 @@
+x=2.6
+print(round(x))
+import math
+print(math.ceil(2.1))
+print(math.floor(2.9))
---
+++ Day3/if_statement.py
@@ -0,0 +1,9 @@
+weather=str(input("enter the weather condition"))
+if weather=="hot":
+    print("It's a hot day")
+    print("Drink plenty of water")
+elif weather=="cool":
+    print("It's a cold day")
+    print("Wear warm clothes")
+else:
+    print("It's a lovely day")
